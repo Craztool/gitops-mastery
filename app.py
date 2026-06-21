4cac43287530f811a2ce77e33e3678925ddc4031
Craztool <ineeddewey@gmail.com>
2026-06-21 22:14:40 +0700

    Test ingress

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ class SimpleHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html; charset=utf-8')
         self.end_headers()
-        html = "<html><body><h1>Test auto robot</p></body></html>"
+        html = "<html><body><h1>Test ingress</h1></p></body></html>"
         self.wfile.write(html.encode('utf-8'))
 
 if __name__ == '__main__':
